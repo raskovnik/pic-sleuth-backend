8e10286126ac35b87819e95b2ba9dc77ee26cc91
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2023-06-07 06:06:45 +0300

    project scaffolding (#1)
    * project scaffolding
    
    * Fix issues raised in code review
    
    * Fix issues raised in code review
    
    * Fix issues raised in code review

---
+++ tests/photos/test_views.py
@@ -0,0 +1,15 @@
+import pytest
+from django.urls import reverse
+from django.http import HttpResponse
+from django.test import RequestFactory
+from pic_sleuth.photos.views import HelloView
+
+class TestHelloView:
+    def test_hello_view_with_get(self):
+        factory = RequestFactory()
+        request = factory.get(reverse("hello"))
+        response = HelloView(request)
+
+        assert response.status_code == 200
+        assert response.content.decode() == "Hello, World!"
+
